ca38053e31e0045c9598b9c86344a942f3f44de8
kawanaka <kawanaka_s@tenso.com>
2018-12-27 17:40:51 +0900

    fixup! fizz_buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -17,3 +17,29 @@ puts fizz_buzz(4)
 puts fizz_buzz(5)
 puts fizz_buzz(6)
 puts fizz_buzz(15)
+
+
+def fizz_buzz(n)
+  if n % 4 == 0 && n % 6 == 0
+    "Fizz Buzz"
+  elsif n % 4 == 0
+    "Fizz"
+  elsif n % 6 == 0
+    "Buzz"
+  else
+    n.to_s
+  end
+end
+    
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(12)
+puts fizz_buzz(15)
+puts fizz_buzz(20)
+

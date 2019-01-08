1571628684beacbfaca8bdb82f3a5d091014b5f0
kawanaka <kawanaka_s@tenso.com>
2019-01-08 17:37:24 +0900

    fizz_buzzの呼び出し方法を範囲指定に変更

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -9,14 +9,8 @@ def fizz_buzz(n)
     n.to_s
   end
 end
-    
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+ 
+(1..1000).each { |n| puts fizz_buzz(n) }
 
 
 def fizz_buzz(n)
@@ -31,15 +25,4 @@ def fizz_buzz(n)
   end
 end
     
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(7)
-puts fizz_buzz(8)
-puts fizz_buzz(12)
-puts fizz_buzz(15)
-puts fizz_buzz(20)
-
+(1..1000).each { |n| puts fizz_buzz(n) }

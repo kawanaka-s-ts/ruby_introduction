6d6ee0a14edc120b073a2f8e82c7cb5214fdec74
kawanaka <kawanaka_s@tenso.com>
2019-01-08 15:25:53 +0900

    word_synthとeffectおよびそれらのテストコード

---
+++ word_synth/effects.rb
@@ -0,0 +1,13 @@
+module Effects
+  def self.reverse
+    ->(words) { words.split(" ").map(&:reverse).join(" ") }
+  end
+
+  def self.echo(rate)
+    ->(words) { words.chars.map { |c| c == " " ? c : c * rate }.join }
+  end
+
+  def self.loud(level)
+    ->(words) { words.split(" ").map { |word| word.upcase + "!" * level }.join(" ") }
+  end
+end

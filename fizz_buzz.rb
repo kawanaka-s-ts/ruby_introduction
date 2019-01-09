c8a08ccbe608e47a1ca89b34638bcf4152d1b403
kawanaka <kawanaka_s@tenso.com>
2019-01-09 13:09:06 +0900

    条件式の数値は引数で指定する様に変更

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -26,3 +26,18 @@ def fizz_buzz(n)
 end
     
 (1..1000).each { |n| puts fizz_buzz(n) }
+
+
+def fizz_buzz(n, a = 4, b = 6)
+  if n % a == 0 && n % b == 0
+    "Fizz Buzz"
+  elsif n % a == 0
+    "Fizz"
+  elsif n % b == 0
+    "Buzz"
+  else
+    n.to_s
+  end
+end
+    
+(1..1000).each { |n| puts fizz_buzz(n) }

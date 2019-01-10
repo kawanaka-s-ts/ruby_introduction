b3b61a2c978501067932f97a7b6c21e855a912e4
kawanaka <kawanaka_s@tenso.com>
2019-01-10 17:16:07 +0900

    DVDクラスを作成
    - DVDクラスはProductクラスを継承しているのでname,priceの属性も扱える
    - スーパークラスでattr_readerが設定されている物は再定義の必要がない

--- super.rb
+++ super.rb
@@ -10,3 +10,18 @@ end
 product = Product.new("A great movie", 1000)
 product.name
 product.price
+
+class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    @name = name
+    @price = price
+    @running_time = running_time
+  end
+end
+
+dvd = DVD.new("A great movie", 1000, 120)
+dvd.name
+dvd.price
+dvd.running_time
